49671565874896fc9fd53635cd57be19323be638
Zheng Chengqi <archyzhengchengqi@gmail.com>
2024-10-12 16:27:11 +0800

    task1 room and random agent and goal finished. next step is to add keys

--- navix/environments/hi_core.py
+++ navix/environments/hi_core.py
@@ -13,7 +13,7 @@ from . import Environment
 from ..entities import Player, Key, Door, Goal, Wall
 from ..states import State
 from . import Timestep
-from ..grid import mask_by_coordinates, room, random_positions, random_directions, random_colour
+from ..grid import mask_by_coordinates, room, random_positions, random_directions, random_colour, RoomsGrid
 from .registry import register_env
 
 
@@ -125,7 +125,7 @@ class Hi_Core_task_1(Environment):
             self.width > 4
         ), f"Room width must be greater than 5, got {self.width} instead"
 
-        key, k1, k2, k3, k4 = jax.random.split(key, 5)
+        key, k1, k2, k3, k4, k5 = jax.random.split(key, 6)
 
         grid = room(height=self.height, width=self.width)
 
@@ -162,11 +162,26 @@ class Hi_Core_task_1(Environment):
             # wall_position_final = jnp.stack((wall_position_final, wall_pos))
         wall_pos = jnp.concatenate(wall_position_final)
         walls = Wall.create(position=wall_pos)
-        
 
+        # separate rooms
+        room_1 = mask_for_task_1(
+            grid, begin_row=1, end_row=2
+        )
+        room_2 = mask_for_task_1(
+            grid, begin_row=4, end_row=5
+        )
+        room_3 = mask_for_task_1(
+            grid, begin_row=7, end_row=8
+        )
+
+        room_2 = jnp.where(room_2, grid, -1) 
+        room_3 = jnp.where(room_3, grid, -1) 
+        
+        # agent and goal
         if self.random_start:
-            player_pos, goal_pos = random_positions(k1, grid, n=2)
-            direction = random_directions(k2, n=1)
+            player_pos = random_positions(k1, room_2)
+            direction = random_directions(k5)
+            goal_pos = random_positions(k2, room_3)
         else:
             goal_pos = jnp.asarray([self.height - 2, self.width - 2])
             player_pos = jnp.asarray([1, 1])
@@ -203,7 +218,18 @@ class Hi_Core_task_1(Environment):
             step_type=jnp.asarray(0, dtype=jnp.int32),
             state=state,
         )
- 
+
+def mask_for_task_1(grid, begin_row, end_row):
+    mesh = jnp.mgrid[0 : grid.shape[0], 0 : grid.shape[1]]
+    cond_1 = jnp.less(mesh[0], end_row + 1)
+    cond_2 = jnp.greater(mesh[0], begin_row - 1)
+    cond_3 = jnp.less(mesh[1], grid.shape[0] - 1)
+    cond_4 = jnp.greater(mesh[1], 0)
+    mask = jnp.asarray(jnp.logical_and(cond_1, cond_2), dtype=jnp.int32)
+    mask_1 = jnp.asarray(jnp.logical_and(cond_3, cond_4), dtype=jnp.int32)
+    final_mask = jnp.asarray(jnp.logical_and(mask, mask_1), dtype=jnp.int32)
+    return final_mask
+
 register_env(
     "Hi_Core_task_0",
     lambda *args, **kwargs: Hi_Core_task_0.create(
@@ -226,7 +252,7 @@ register_env(
         termination_fn=kwargs.pop("termination_fn", terminations.on_goal_reached),
         height=10,
         width=10,
-        random_start=False,
+        random_start=True,
         *args,
         **kwargs,
     ),
